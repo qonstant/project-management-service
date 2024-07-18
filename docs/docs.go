3d41a205a531a74130748342fa0ea0abfa1ea8d6
Rakymzhan Zhabagin <rakorako2204@gmail.com>
2024-07-18 14:58:58 +0500

    swagger fix

--- docs/docs.go
+++ docs/docs.go
@@ -940,8 +940,6 @@ var SwaggerInfo = &swag.Spec{
 	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
-	// LeftDelim:        "{{",
-	// RightDelim:       "}}",
 }
 
 func init() {
